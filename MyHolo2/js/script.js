405dfaba29e46b0a6c51e7bee20c9f7057fd9039
OtmarJe <o.jenner@gmx.de>
2021-02-07 01:20:17 +0100

    grid adjust

--- MyHolo2/js/script.js
+++ MyHolo2/js/script.js
@@ -1,1 +1,11 @@
-//var 
+import { wrapGrid } from 'animate-css-grid'
+ 
+const grid = document.querySelector(".grid");
+const { unwrapGrid, forceGridAnimation } = wrapGrid(grid);
+
+var element = document.getElementById("wrapGrid");
+element.addEventListener("animationstart", listener, false);
+element.addEventListener("animationend", listener, false);
+element.addEventListener("animationiteration", listener, false);
+
+element.className = "grid";
